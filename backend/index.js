301f6d43f023eb323696595017e244bc7ef2ed70
vanya2535 <ncab_03@mail.ru>
2021-11-14 00:36:54 +0300

    backend cors fix

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const authRouter = require('./router/authRouter.js')
 const roleRouter = require('./router/roleRouter.js')
 
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  credentials: true,
+  optionSuccessStatus: 200
+}
 const app = express()
 
 const PORT = 3000
@@ -10,6 +16,7 @@ const DB_URL =
   'mongodb+srv://dbUser:dbUserPassword@shopdb.yr5gu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 app.use(express.json())
+app.use(cors(corsOptions))
 app.use('/user', authRouter)
 app.use('/role', roleRouter)
 
