28ad82227acdef6dfe1dea4624e15aeb5bde1d5a
vanya2535 <ncab_03@mail.ru>
2021-11-13 18:05:39 +0300

    router update

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '@/store'
+import Index from '@/views/pages/index/index'
 
 Vue.use(VueRouter)
 
 const routes = [
-  //
+  {
+    path: '/',
+    name: 'Index',
+    component: Index,
+    meta: {
+      auth: false
+    }
+  }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (!store.getters['user/IS_LOGGED'] && to.meta.auth) {
+    next({ name: 'Index' })
+  } else if (to.name) {
+    next()
+  } else {
+    next({ name: 'Index' })
+  }
 })
 
 export default router
