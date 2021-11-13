2895ee3fc7460490476a3d971f8b27934347b9c4
vanya2535 <ncab_03@mail.ru>
2021-11-14 00:10:37 +0300

    axios plugin added

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from './plugins/axios.js'
 import commonComponents from './components/common/index.js'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$api = axios
 
 for (let component of commonComponents) {
   Vue.component(component.name, component)
---
+++ src/plugins/axios.js
@@ -0,0 +1,5 @@
+import Axios from 'axios'
+
+Axios.defaults.baseURL = 'http://localhost:3000/'
+
+export default Axios
