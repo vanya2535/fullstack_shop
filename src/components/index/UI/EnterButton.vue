56f5aa9d558e25e56f004933966da5f16a3c9c79
vanya2535 <ncab_03@mail.ru>
2021-11-13 18:04:38 +0300

    enter_button-component created

---
+++ src/components/index/UI/EnterButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="enter-button" @click="$emit('click')">
+    <h6>{{ innerText }}</h6>
+  </button>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'EnterButton',
+
+  computed: {
+    ...mapGetters('user', ['IS_LOGGED']),
+    innerText() {
+      return this.IS_LOGGED ? 'Go shopping' : 'Login'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.enter-button {
+  @include border;
+
+  border-radius: 24px;
+  padding: 10px 64px;
+  color: $white;
+  background-color: $primary;
+}
+</style>
