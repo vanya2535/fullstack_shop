a0e34d32c981abcdf57efb8a7f50b699c6acca4f
vanya2535 <ncab_03@mail.ru>
2021-11-13 18:00:54 +0300

    configiguration update, vuetify remove

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,22 @@ module.exports = {
   },
   rules: {
     'vue/attribute-hyphenation': 0,
+    "vue/attributes-order": ["error", {
+      "order": [
+        "DEFINITION",
+        "LIST_RENDERING",
+        "CONDITIONALS",
+        "RENDER_MODIFIERS",
+        "GLOBAL",
+        ["UNIQUE", "SLOT"],
+        "TWO_WAY_BINDING",
+        "OTHER_DIRECTIVES",
+        "OTHER_ATTR",
+        "EVENTS",
+        "CONTENT"
+      ],
+      "alphabetical": false
+    }],
     'comma-dangle': ['error', 'never'],
     'no-unused-vars': 0
   }
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  transpileDependencies: ['vuetify']
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg')
+
+    svgRule.uses.clear()
+
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "~@/assets/scss/core/variables.scss";
+          @import "~@/assets/scss/mixins/border.scss";
+        `
+      }
+    }
+  }
 }
