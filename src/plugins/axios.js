28eae474a789b327285ec0f3588df2b2854365f9
vanya2535 <ncab_03@mail.ru>
2021-11-15 18:25:19 +0300

    axios authorization header

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,5 +1,8 @@
 import Axios from 'axios'
 
+const user = JSON.parse(localStorage.getItem('user'))
+
 Axios.defaults.baseURL = 'http://localhost:3000/'
+Axios.defaults.headers = { Authorization: user._id }
 
 export default Axios
